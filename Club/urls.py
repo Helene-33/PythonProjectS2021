4af2e01aa409c69c4dc1db90d2a8edbe810cd7f2
Helene Liobard <heleneliobard@hotmail.com>
2021-05-13 14:59:37 -0700

    views assignment

--- Club/urls.py
+++ Club/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('meetings/', views.meetings, name='meetings'),
+    path('meetingsminutes/', views.meetingsminutes, name='meetingsminutes'),
+    path('resources/', views.resources, name='resources'),
+    path('events/', views.events, name='events'),
 ]
--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event 
 
 # Create your views here.
 def index (request):
     return render(request, 'Club/index.html')
+
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'Club/meetings.html', {'meeting_list': meeting_list})
+
+def meetingsminutes(request):
+    meetingminutes_list=MeetingMinutes.objects.all()
+    return render(request, 'Club/meetings.minutes.html', {'meetingminutes_list': meetingminutes_list})
+
+def resources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'Club/resources.html', {'resource_list': resource_list})
+
+def events(request):
+    event_list=Event.objects.all()
+    return render(request, 'Club/events.html', {'event_list': event_list})
